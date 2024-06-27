d0264bbbf96c3ac2faf9f8fee5936b6071882e05
Ernando Braga da Silva <nandobraga78@gmail.com>
2024-06-27 08:23:05 -0300

    aula de introdução a nodemon

---
+++ node/aula8 express+Nodemon/server.js
@@ -0,0 +1,39 @@
+
+/**
+ * Quando o cliente tenta acessar o site ele faz uma requisição http://meusite.com/ <- GET -> Entregue a página HTML
+ * 
+ *          Criar   ler   atualizar  deletar
+ * CRUD -> CREATE, READ,   UPDATE,   DELETE
+ * CREATE -> POST -> CADASTRAR OU ENVIAR DADOS
+ * READ -> GET -> LER OU CONSULTAR
+ * UPDATE -> PUT -> ATUALIZAR OU EDITAR DADOS
+ * DELETE -> DELETE -> DELETAR OU EXCLUIR DADOS 
+ */
+
+const express = require('express');
+const send = require('send');
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.send(`
+    <form action="/" method="POST">
+    Nome do Cliente: <input type="text" name="nome">
+    
+    <button>Enviar</button>
+    </form>
+    `);
+});
+app.post('/', (req, res) => {
+    res.send('Recebi o formulário');
+});
+
+app.get('/contato', (req, res) => {
+    res,send('<b>Obrigado por entra em contato com a gente.</b>');
+    
+})
+
+app.listen(3000, () => {
+    console.log('Acessar http://localhost:3000');
+    console.log('Server running on port 3000');
+});
