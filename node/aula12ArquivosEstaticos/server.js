0fc18ed7e4c657ce4c3499c6d8c7806d4b1d9b02
Ernando Braga da Silva <nandobraga78@gmail.com>
2024-07-01 14:29:50 -0300

    conhecendo expressviews e arquivoEstaticosExpress

---
+++ node/aula12ArquivosEstaticos/server.js
@@ -0,0 +1,32 @@
+/**
+ * Trabalhando com aplicação com rota
+ * exemplo uma rota -> //http://facebook.com/profiles/
+ * na rota a unica coisa que muda e o paramêntro da rota ou seja ser http://facebook.com/profiles/123
+ * neste caso o que foi mudado será o 123 que é o id
+ * outro tipo de paramêntro que pode ser usado e o quyery string que é o que vem depois do primeiro /
+ * para começa o quyery string tem que colocar o ?
+ * exemplo http://facebook.com/profiles/?id=123?campanha=googleads
+ * a quyery string é um par de chave e valor
+ * Quando estive mas de quyery string que é um par de chave e valor eu uso o &
+ * exemplo http://facebook.com/profiles/?id=123?campanha=googleads&nome_cqampanha=seila
+ */
+
+const express = require("express");
+const send = require("send");
+const app = express();
+const routes = require("./routes");
+const path = require("path");
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.set('views', path.resolve(__dirname, 'src', 'views'));
+app.set('view engine', 'ejs');
+
+app.use(routes);
+
+app.listen(3000, () => {
+  console.log("Acessar http://localhost:3000");
+  console.log("Server running on port 3000");
+});
